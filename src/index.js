5a522e35005703e1cce2a8c2a6a23741f08404b2
Benjamin <Officialbenragland@gmail.com>
2024-02-29 17:52:27 -0500

    adds edit button

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ const centerRestaurantName = document.querySelector("#ramen-detail h3")
 
 const dishRating = document.querySelector("#rating-display")
 const dishComment = document.querySelector("#comment-display")
-let centerId = 0;
+//Div holds all ramens on screen
+const allRamensDisplayList = document.querySelector("#ramen-menu")
+
+//connect to Form
+const submitForm = document.getElementById("new-ramen")
+
+let editMode = false
+
+
 
 // Callbacks
 const handleClick = (e,ramen) => {
-  
+
   //set Center Image and text
   centerImage.src = e.target.src
   centerDishName.textContent = ramen.name
@@ -19,18 +27,24 @@ const handleClick = (e,ramen) => {
 
   dishRating.textContent = ramen.rating
   dishComment.textContent = ramen.comment
-  centerId = ramen.id
+  centerImage.setAttribute("alt", `${ramen.id}`) 
+
+  //clear edit form on click of new dish
+  editMode = false
+  console.log("edit Mode on ramen menu click --->", editMode)
+  submitForm.name.placeholder = ""
+  submitForm.restaurant.placeholder = ""
+  submitForm["new-image"].placeholder = ""
+  submitForm.rating.placeholder = " "
+  submitForm["new-comment"].placeholder = " "
 };
 
 const addSubmitListener = () => {
-  // Connect to DOM
-  const submitButton = document.getElementById("submitButton")
-  const submitForm = document.getElementById("new-ramen")
-  
+  if (!submitForm){
+    return  
+  }
   submitForm.addEventListener("submit", (e) => {
     e.preventDefault()
-
-    console.log(e.target.image.value)
 
     const newDish = {
       name: e.target.name.value ,
@@ -53,18 +67,38 @@ const addSubmitListener = () => {
       if (res.ok){
         return res.json() 
       }else {
-        alert("server response error --->",res)
+        alert(`server response error, submitListener ---> ${res}`)
       }
     })
     .then(ramen => {
       console.log("POST RESPONSE -->" , ramen)
     })
     .catch( e => {
-      alert("server error ---->",e)
+      alert(`server error ----> submitListener , ${e}`)
     })
 
 
   })
+//Need to find centerImageID
+  console.log("center Image Id -->", centerImage)
+  if (editMode === true){
+    console.log("edit mode true" , editMode)
+    //need to get correct ID
+    fetch(`http://localhost:3000/ramens/${centerImage.id}`, {
+      method:"PATCH",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify({
+        name: submitForm.name.value,
+        restaurant: submitForm.restaurant.value,
+        image:submitForm["new-image"].src,
+        rating: submitForm.rating.value,
+        comment:submitForm["new-comment"].value
+      })
+
+  })
+  }
 
 }
 
@@ -75,39 +109,38 @@ const displayRamens = () => {
     if (res.ok){
       return res.json() 
     }else {
-      alert("server response error --->",res)
+      alert(`server error ----> display , ${e}`)
     }
   })
   .then(ramens => {
+    // Access DOM
+    const ramenMenuDiv = document.getElementById("ramen-menu")
+   
     ramens.forEach((ramen) => {
-
-      // Access DOM
-      const ramenMenuDiv = document.getElementById("ramen-menu")
-      
+        
       // created stuff
-      const ramenListUL = document.createElement("ul")
-      const ramenListLI = document.createElement("li")
       const ramenImage = document.createElement("img")
-      
       
       //add data to created stuff
       ramenImage.className = "ramen-img"
       ramenImage.src = ramen.image
+      ramenImage.id = ramen.id
+      ramenImage.dishName = ramen.name
+      ramenImage.restaurant = ramen.restaurant
+      ramenImage.rating = ramen.rating
+      ramenImage.comment = ramen.comment
       
       //append stuff
-      ramenMenuDiv.appendChild(ramenListUL)
-      ramenListLI.append(ramenImage)
-      ramenListUL.append(ramenListLI)
-      
-      //when Clicking on ramen
-      ramenListLI.addEventListener("click", (e) => handleClick(e , ramen))
-      
-      
+        ramenMenuDiv.appendChild(ramenImage)
+
+      //when Clicking on a ramen img
+      ramenImage.addEventListener("click", (e) => handleClick(e , ramen))
     })
     
   })
   .catch( e => {
-    alert("server error ---->",e)
+    console.log(e)
+    alert(`server error ----> displayramens , ${e}` )
   })
   
 };
@@ -120,43 +153,85 @@ const displayCenterFirst = () => {
     if (res.ok)
       return res.json()
     else{
-      alert("server response error ===>", res)
+      alert(`server response error, displayCenterFirst ===> ${res}`)
     }
   })
   .then(ramens => {
     if (centerDishName.textContent === "Insert Name Here"){
       
-      centerImage.src = ramens[0].image
-      centerDishName.textContent = ramens[0].name
-      centerRestaurantName.textContent = ramens[0].restaurant
-      
-      dishRating.textContent = ramens[0].rating
-      dishComment.textContent = ramens[0].comment
-      
-      centerId = ramens[0].id
-      
-      
+      if(centerDishName){
+        centerImage.src = ramens[0].image
+        centerDishName.textContent = ramens[0].name
+        centerRestaurantName.textContent = ramens[0].restaurant
+        
+        dishRating.textContent = ramens[0].rating
+        dishComment.textContent = ramens[0].comment
+
+      }
+    
     }
   })
 }
 
 
 // WARNING: Will Persist if uncommented. Check backupDB if needed.
-
 const deleteDish = () =>{
 
+    document.getElementById("delete-btn").addEventListener("click",(e)=>{    
+  
+      for ( let img of allRamensDisplayList.children){
+        if (img.id === centerImage.id){
+          img.remove()
+          //delete center image and details
+          alert(`${centerDishName.textContent} Removed`)
+          centerImage.src =  "./assets/image-placeholder.jpg"
+          centerDishName.textContent = ""
+          centerRestaurantName.textContent = ""
+          dishRating.textContent =""
+          dishComment.textContent=""
 
-    document.getElementById("delete-btn").addEventListener("click",()=>{
-
-  //   fetch(`http://localhost:3000/ramens/${centerId}`,{
-  //       method:"DELETE",
-  //   })
-  //   .then(res => res.json())
-  //   .then(ramen => console.log(`ramen id:${ramen.id} deleted`))
+          // fetch(`http://localhost:3000/ramens/${img.id}`,{
+          //     method:"DELETE",
+          // })
+          // .then(res => res.json())
+          // .then(ramen => console.log(`ramen id:${ramen.id} deleted`))
+        }
+      }
+   
     })
   
 }
 
+
+function editRamen(){
+  const editBtn = document.getElementById("edit-btn").addEventListener("click",() =>{
+    if (editMode === true){
+      editMode = false
+
+    //clear edit form on re-click of edit button
+    console.log("edit Mode on ramen menu click --->", editMode)
+    submitForm.name.placeholder = ""
+    submitForm.restaurant.placeholder = ""
+    submitForm["new-image"].placeholder = ""
+    submitForm.rating.placeholder = " "
+    submitForm["new-comment"].placeholder = " "
+    
+  }else{
+      editMode = true
+    }
+    console.log("Edit Mode --->", editMode)
+    if (editMode === true){
+      submitForm.name.placeholder = centerDishName.textContent
+      submitForm.restaurant.placeholder = centerRestaurantName.textContent
+      submitForm["new-image"].placeholder = `.${centerImage.src.slice(21)}`
+      submitForm.rating.placeholder = dishRating.textContent
+      submitForm["new-comment"].placeholder = dishComment.textContent 
+    }
+
+
+  })
+
+}
 
 const main = () => {
   // Invoke displayRamens here
@@ -168,8 +243,8 @@ const main = () => {
   // Center Display 1st Obj in Dish List on Screen load
   displayCenterFirst()
   deleteDish()
+  editRamen()
 }
-
 main()
 
 
@@ -183,4 +258,5 @@ export {
 
 
 //Create EditMode Boolean
-//Create DeleteButton #delete-btn + #edit-btn created
+//Create DeleteButton #delete-btn + #edit-btn created-->> use global variables
+//centerimage ,center dishname 
